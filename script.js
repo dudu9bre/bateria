e279b9f7653c067e83b4ac73b7819a8e98f64c1d
Carlos <148721970+dudu9bre@users.noreply.github.com>
2024-02-21 22:36:17 -0300

    testeeee

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*
+
 function removeTransition(e) {
     if (e.propertyName !== "transform") return
     if (!{}[e.keyCode]) {
@@ -22,3 +24,32 @@ const keys = Array.from(document.querySelectorAll(".key"))
 
 keys.forEach(key => addEventListener("transitionend", removeTransition))
 window.addEventListener("keydown", playSound)
+
+*/
+
+
+function playSound(e) {
+    const key = e.type === 'keydown' ? e.keyCode : e.target.getAttribute('data-key');
+    const audio = document.querySelector(`audio[data-key="${key}"]`);
+    const div = document.querySelector(`div[data-key="${key}"]`);
+    if (!audio) return;
+
+    div.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+
+    audio.onended = function() {
+        div.classList.remove('playing');
+    };
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+}
+
+window.addEventListener('keydown', playSound);
+Array.from(document.querySelectorAll('div[data-key]')).forEach(item => {
+    item.addEventListener('touchstart', playSound);
+    item.addEventListener('transitionend', removeTransition);
+});
