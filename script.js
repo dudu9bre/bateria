8bebddacfbf35b198779e1c63d0069c88e6639aa
Carlos <148721970+dudu9bre@users.noreply.github.com>
2024-02-21 22:56:04 -0300

    agr vai

--- script.js
+++ script.js
@@ -27,29 +27,56 @@ window.addEventListener("keydown", playSound)
 
 */
 
-
+/*
 function playSound(e) {
-    const key = e.type === 'keydown' ? e.keyCode : e.target.getAttribute('data-key');
-    const audio = document.querySelector(`audio[data-key="${key}"]`);
-    const div = document.querySelector(`div[data-key="${key}"]`);
-    if (!audio) return;
+    const key = e.type === 'keydown' ? e.keyCode : e.target.getAttribute('data-key')
+    const audio = document.querySelector(`audio[data-key="${key}"]`)
+    const div = document.querySelector(`div[data-key="${key}"]`)
+    if (!audio) return
 
-    div.classList.add('playing');
+    div.classList.add('playing')
     audio.currentTime = 0;
-    audio.play();
+    audio.play()
 
     audio.onended = function() {
-        div.classList.remove('playing');
+        div.classList.remove('playing')
     };
 }
 
 function removeTransition(e) {
-    if (e.propertyName !== 'transform') return;
-    e.target.classList.remove('playing');
+    if (e.propertyName !== 'transform') return
+    e.target.classList.remove('playing')
 }
 
-window.addEventListener('keydown', playSound);
+window.addEventListener('keydown', playSound)
 Array.from(document.querySelectorAll('div[data-key]')).forEach(item => {
-    item.addEventListener('touchstart', playSound);
-    item.addEventListener('transitionend', removeTransition);
-});
+    item.addEventListener('touchstart', playSound)
+    item.addEventListener('transitionend', removeTransition)
+})
+*/
+
+function playSound(e) {
+    const key = e.type === 'keydown' ? e.keyCode : e.target.closest('div').getAttribute('data-key')
+    const audio = document.querySelector(`audio[data-key="${key}"]`)
+    const div = document.querySelector(`div[data-key="${key}"]`)
+    if (!audio) return
+
+    div.classList.add('playing')
+    audio.currentTime = 0
+    audio.play()
+
+    audio.onended = function() {
+        div.classList.remove('playing')
+    }
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return
+    e.target.classList.remove('playing')
+}
+
+window.addEventListener('keydown', playSound)
+Array.from(document.querySelectorAll('div[data-key]')).forEach(item => {
+    item.addEventListener('touchstart', playSound)
+    item.addEventListener('transitionend', removeTransition)
+})
